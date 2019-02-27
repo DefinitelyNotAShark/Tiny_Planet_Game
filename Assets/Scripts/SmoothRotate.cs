e3534d9843305abc0c1040625e8abf095224c09b
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-02-26 18:30:46 -0600

    FUCKING SMOOTH ROTATION MY DUDES

---
+++ Assets/Scripts/SmoothRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothRotate : MonoBehaviour
+{
+	void Update ()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float heading = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+
+        transform.localEulerAngles = new Vector3(0, heading, 0);
+        //Quaternion rotation = Quaternion.Euler(0, heading, 0);
+
+        //transform.rotation = rotation;
+    }
+}
