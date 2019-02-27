09c1c3214d525186cc7955c1e380746a521f3ae2
Ja'ire <jv9ufxcy@yahoo.com>
2019-02-27 02:31:19 -0600

    adjusted gun delay coroutine, firing gun now fires multiple pellets in a spread
    Also adjusted animation timing and placed the bullet spawn location on the global controller

--- Assets/Scripts/PlayerScripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerScripts/PlayerShoot.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [Header("Bullet Settings")]
     [SerializeField]
     private GameObject bulletPrefab;
-
     [SerializeField]
-    private float bulletSpeed, bulletLifetime;
+    private Transform bulletMuzzle;
+    [SerializeField]
+    private float bulletSpeed, bulletLifetime, shotDelay=0f, shotCooldown=0.5f;
+    [Header("Shotgun Spread Settings")]
+    [SerializeField]
+    private int bulletCount;
+    [SerializeField]
+    private float spreadAngle;
+    List<Quaternion> pellets;
 
     private GameObject bulletInstance;
     private Animator anim;
 
     private bool coroutineStarted;
-    
+
+    private void Awake()
+    {
+        pellets = new List<Quaternion>(new Quaternion[bulletCount]);
+    }
     private void Start()
     {
-        anim = GetComponentInParent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update ()
@@ -26,7 +38,8 @@ public class PlayerShoot : MonoBehaviour
         {
             if (!coroutineStarted)
             {
-                StartCoroutine(ShootWithAnimation());
+                Shoot();
+                StartCoroutine(ShootingCooldown());
                 anim.SetBool("isShooting", true);
             }
         }
@@ -37,21 +50,28 @@ public class PlayerShoot : MonoBehaviour
     void Shoot()
     {
         //AUDIO shooting sound
-        bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);
-        bulletInstance.AddComponent<MoveBullet>().speed = bulletSpeed;
-        bulletInstance.GetComponent<MoveBullet>().lifetime = bulletLifetime;
+        int i = 0;
+        foreach (Quaternion quat in pellets)
+        {
+            pellets[i] = Random.rotation;
+            bulletInstance = Instantiate(bulletPrefab, bulletMuzzle.position, bulletMuzzle.rotation);
+            bulletInstance.transform.rotation = Quaternion.RotateTowards(bulletInstance.transform.rotation, pellets[i], spreadAngle);
+            bulletInstance.AddComponent<MoveBullet>().speed = bulletSpeed;
+            bulletInstance.GetComponent<MoveBullet>().lifetime = bulletLifetime;
+            i++;
+        }
     }
 
-    private IEnumerator ShootWithAnimation()
+    private IEnumerator ShootingCooldown()
     {
         coroutineStarted = true;
-        yield return new WaitForSeconds(.2f);
+        //yield return new WaitForSeconds(shotDelay);
+        
+        //bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        //bulletInstance.AddComponent<MoveBullet>().speed = bulletSpeed;
+        //bulletInstance.GetComponent<MoveBullet>().lifetime = bulletLifetime;
 
-        bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);
-        bulletInstance.AddComponent<MoveBullet>().speed = bulletSpeed;
-        bulletInstance.GetComponent<MoveBullet>().lifetime = bulletLifetime;
-
-        yield return new WaitForSeconds(.5f);//tiny cooldown
+        yield return new WaitForSeconds(shotCooldown);//tiny cooldown
         coroutineStarted = false;
     }
 }
