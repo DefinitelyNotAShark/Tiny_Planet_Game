556c7c3fb5357a6614bd4c42e519f404e58fbf01
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-02-27 13:51:20 -0600

    Enemy Knockback

--- Assets/Scripts/EnemyScripts/EnemyDetectColl.cs
+++ Assets/Scripts/EnemyScripts/EnemyDetectColl.cs
@@ -8,13 +8,16 @@ public class EnemyDetectColl : MonoBehaviour
     private int damageCoolDown;
     private bool coroutineStarted;
 
+    private Rigidbody rigidbody;
+
     private void Start()
     {
         coroutineStarted = false;
         damageAmount = 10;
         damageCoolDown = 1;
 
-        Physics.IgnoreCollision(GetComponent<Collider>(), GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>());
+        //Physics.IgnoreCollision(GetComponent<Collider>(), GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>());
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -44,10 +47,18 @@ public class EnemyDetectColl : MonoBehaviour
     private IEnumerator HurtPlayer(Collider other)
     {
         coroutineStarted = true;
-
+        DoKnockBack(other);//pass in the player coll so it can get the player position
         other.gameObject.GetComponentInChildren<PlayerHealth>().HurtPlayer(damageAmount);//take player health down by the amount (damage amount)
 
         yield return new WaitForSeconds(damageCoolDown);
         coroutineStarted = false;
+    }
+
+   void DoKnockBack(Collider playerColl)
+    {
+        Transform playerTransform = playerColl.gameObject.transform;
+
+        Vector3 direction = transform.position - playerTransform.position;
+        rigidbody.AddForce(direction.normalized * 1000f);
     }
 }
